ab34eafb318f2e7980c1d98fd3e372567141348d
Arnaud293 <arnaudlt293@gmail.com>
2023-01-03 12:37:36 +0100

    Create a Layout who render childrens always  below the header

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,9 +1,17 @@
-import 'tailwindcss/tailwind.css'
-import '../styles/globals.scss'
+import React, { useState, useEffect } from 'react';
+
+import 'tailwindcss/tailwind.css';
+import '../styles/globals.scss';
+
+import { Layout } from '../components';
 
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout> 
+  )
 }
 
 export default MyApp
