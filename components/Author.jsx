f6801fcfbb139536bdaba36fcae5f093b17b26f2
Arnaud293 <arnaudlt293@gmail.com>
2023-01-04 12:11:14 +0100

    render author details

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
-const Author = () => {
+const Author = ({ author }) => {
+  console.log(author);
   return (
-    <div>Author</div>
-  )
-}
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-purple-800 bg-opacity-20">
+      <div className="absolute left-0 right-0 -top-14 flex w-full justify-center">
+        <Image
+          unoptimized={true}
+          src={author.photo.url}
+          alt={author.name}
+          height={`${100}`}
+          width={`${100}`}
+          className="align-middle rounded-full"
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
+    </div>
+  );
+};
 
-export default Author
+export default Author;
