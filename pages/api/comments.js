1e607e656feaaf594ddd10fc5bea52a19eefa9ff
Arnaud293 <arnaudlt293@gmail.com>
2023-01-05 12:37:11 +0100

    post comment

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import { GraphQLClient, gql } from "graphql";
+import { GraphQLClient, gql } from "graphql-request";
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
 
 
 export default async function comments(
@@ -10,17 +11,22 @@ export default async function comments(
   res
 ) {
   const { name, email, slug, comment } = req.body;
-  const graphQLClient = new GraphQLClient(graphqlAPI, {
+  const graphQLClient = new GraphQLClient((graphqlAPI), {
     headers: {
-      authorization : `Bearer ${process.env.GRAPHCMS_TOKEN}`
+      authorization : `Bearer ${graphcmsToken}`
     }
-  })
+  });
   const query = gql`
   mutation createComment($name: String!, $email: String!, $comment: String!, $slug: String!){
     createComment(data: {name : $name, email: $email, comment: $comment, post: {connect: {slug: $slug}}}){id}
   }
-  `
-  const result = await graphQLClient.request(query, req.body);
-
-  return res.status(200).send(result);
+  `;
+  try {
+    const result = await graphQLClient.request(query, req.body);
+  
+    return res.status(200).send(result);    
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);    
+  }
 }
