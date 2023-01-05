19d315782d823fa1a89ee0c271ecf67a395fdb3e
Arnaud293 <arnaudlt293@gmail.com>
2023-01-05 17:06:54 +0100

    render featured post + logic + responsive

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import { PostCard, Categories, PostWidget } from '../components';
+import FeaturedPosts from '../sections/FeaturedPosts';
 import { getPosts } from '../services';
 
 
@@ -13,6 +14,7 @@ const Home = ({ posts }) => {
         <title>Alt293 Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <FeaturedPosts />
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className='lg:col-span-8 col-span-1'>
           {posts.map((post, index) => (
