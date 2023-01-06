4f3ada5209bbf6c8bf49d22d8c4e359158f5c35c
Arnaud293 <arnaudlt293@gmail.com>
2023-01-06 09:50:59 +0100

    add loader if fallback === true

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { getPosts, getPostDetails } from '../../services';
 
-import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm} from '../../components';
+import {PostDetail, Categories, PostWidget, Author, Comments, CommentsForm, Loader} from '../../components';
 import postcss from 'postcss';
+import { useRouter } from 'next/router';
 
 const PostDetails = ({ post }) => {
-    console.log(post.categories);
+    const router = useRouter();
+
+    if(router.isFallback){
+        return <Loader />
+    }
   return (
     <div className='container mx-auto px-10 mb-8'>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
